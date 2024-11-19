7aa14a3da3e854ff413000f06189ace9ada55a20
Deivid.Godoi <167764531+DeividOFC@users.noreply.github.com>
2024-11-19 11:46:40 -0300

    feature/peso

--- Média.py
+++ Média.py
@@ -2,7 +2,7 @@ nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 
-media = (nota1 + nota2 + nota3) / 3
+media = (nota1 *1+ nota2 *1+ nota3 *2) / (4)
 
 print(f"Média: {media:.2f}")  
 
