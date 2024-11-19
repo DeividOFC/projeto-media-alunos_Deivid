dd6873ce94107fca13ce33a71bb0d71087655435
Deivid.Godoi <167764531+DeividOFC@users.noreply.github.com>
2024-11-19 09:16:46 -0300

    feature/verificacao-recuperacao

--- Média.py
+++ Média.py
@@ -1,11 +1,14 @@
-nota1 = float(input("Digite a nota 1: "))
-nota2 = float(input("Digite a nota 2: "))
-nota3 = float(input("Digite a nota 3: "))
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
 
 media = (nota1 + nota2 + nota3) / 3
 
-print(f"A média é: {media:.2f}")
+print(f"Média: {media:.2f}")  
+
 if media > 6:
     print("Aprovado")
+elif 5.0 < media <= 6.0:
+    print("Recuperação")
 else:
     print("Reprovado")
